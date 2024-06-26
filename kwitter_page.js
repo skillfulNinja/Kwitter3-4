c801fae33a08b38215b6ff6c5b3a6e85f241c4a8
skillfulNinja <154420381+skillfulNinja@users.noreply.github.com>
2024-06-26 20:03:38 +1000

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,24 @@
+//YOUR FIREBASE LINKS
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
+
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "kwitter.html";
+}
